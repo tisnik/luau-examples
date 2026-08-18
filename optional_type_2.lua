b3737763c1958d6933421d605962c1e3b7686328
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 09:51:03 +0200

    Optional data types, example #2

---
+++ optional_type_2.lua
@@ -0,0 +1,14 @@
+--!strict
+
+function add(x: number, y: number, z: number?): number
+    if z == nil then
+        return x + y
+    else
+        return x + y + z
+    end
+end
+
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
+print(add(1, 2, 3, 4))
