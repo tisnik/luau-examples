feb168dee7a508fe0980e546d794057ca36e09d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- table_type_9.lua
+++ table_type_9.lua
@@ -1,5 +1,14 @@
 --!strict
 
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 type Point2D = {x:string, y:string}
 type Point3D = {x:number, y:number, z:number}
 
