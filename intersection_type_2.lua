e80c99305d0465313df80b14400dbf82d1486627
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 09:51:03 +0200

    Intersection data types, example #2

---
+++ intersection_type_2.lua
@@ -0,0 +1,14 @@
+--!strict
+
+type XCoord = {x: number}
+type YCoord = {y: number}
+type ZCoord = {z: number}
+
+type Vector2 = XCoord & YCoord
+type Vector3 = XCoord & YCoord & ZCoord
+
+local vec2: Vector2 = {x = 1, y = 2}
+local vec3: Vector3 = {x = 1, y = 2, z = 3}
+
+print(vec2.x, vec2.y)
+print(vec3.x, vec3.y, vec3.z)
