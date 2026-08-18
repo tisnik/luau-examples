b554ec7af561800d51e3db7cd19e56bb9f0b9b48
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 17:56:22 +0200

    Improper type intersection

---
+++ intersection_type_5.lua
@@ -0,0 +1,10 @@
+--!strict
+
+type Coord1 = {x: number, y: boolean}
+type Coord2 = {y: number, z: number}
+
+type Vector3 = Coord1 & Coord2
+
+local vec3: Vector3 = {x = 1, y = 2, z = 3}
+
+print(vec3.x, vec3.y, vec3.z)
