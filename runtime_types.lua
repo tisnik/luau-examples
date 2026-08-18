73cf233b98f48bb5f4df373a75689674a9681e1b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- runtime_types.lua
+++ runtime_types.lua
@@ -1,3 +1,12 @@
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 v1 = nil
 v2 = true
 v3 = 123
