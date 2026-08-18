beafcad07e602cd0fc3c680a6faf40e8b22e47c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- table_type_2.lua
+++ table_type_2.lua
@@ -1,5 +1,14 @@
 --!strict
 
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 function sum(t: {number}): number
     local s = 0
     for i = 1, #t do
