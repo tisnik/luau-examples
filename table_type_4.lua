9613a685f8954997048ebfb2f8642f5592cc14d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- table_type_4.lua
+++ table_type_4.lua
@@ -1,5 +1,14 @@
 --!strict
 
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 local t : {x:number, y:number, z:number} = {x=1, y=2, z=3}
 
 print(t.x)
