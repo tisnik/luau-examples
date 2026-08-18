74d2c4530d4d47be50b71d0869c65c342c52038a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- simple_buffer.lua
+++ simple_buffer.lua
@@ -1,3 +1,12 @@
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 local b = buffer.create(8)
 
 buffer.writeu8(b, 0, 42)
