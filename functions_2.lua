2374d5d7aa94eb70e97338518a7d3c6b8942dfff
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-18 10:16:13 +0200

    Added description

--- functions_2.lua
+++ functions_2.lua
@@ -1,5 +1,14 @@
 --!strict
 
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 function swap(x:number, y:number):(number,number)
     return y, x
 end
