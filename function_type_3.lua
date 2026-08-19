88c85c708a0f18a6b709211e875c099c431253ca
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-19 09:21:45 +0200

    Fixed function name

--- function_type_3.lua
+++ function_type_3.lua
@@ -1,10 +1,10 @@
 --!strict
 
-function no_args(x: number)
-    print("no_args called with", x)
+function no_return_value(x: number)
+    print("no_return_value called with", x)
 end
 
 local x: (number) -> ()
 
-x = no_args
+x = no_return_value
 x(42)
