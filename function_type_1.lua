1d78f3e039628ac8d704cbec3ed84304994e8a34
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-20 14:01:49 +0200

    Added link to article where the example is used

--- function_type_1.lua
+++ function_type_1.lua
@@ -1,5 +1,14 @@
 --!strict
 
+--
+-- Demonstrační příklad použitý v článku:
+--    Luau: rozšíření jazyka Lua o statické typové kontroly a typovou inferenci (dokončení)
+--    https://www.root.cz/clanky/luau-rozsireni-jazyka-lua-o-staticke-typove-kontroly-a-typovou-inferenci-dokonceni/
+--
+-- Repositář:
+--    https://github.com/tisnik/luau-examples
+--
+
 function zero(x: number): number
     return 0
 end
